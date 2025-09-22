8b2332d4f6c0708715391bc94b24662f48c8a78d
prantysaha44 <2024000000044@seu.edu.bd>
2025-09-23 04:10:52 +0600

    Update Gauss_Seidel_and_SOR.cpp

--- Gauss_Seidel_and_SOR.cpp
+++ Gauss_Seidel_and_SOR.cpp
@@ -17,3 +17,21 @@
         if (maxErr < tol) break;
     }
     return x;
+}
+
+
+vector<double> SORDetailed(const vector<vector<double>>& A, const vector<double>& b, double tol, double w, int& iter) {
+    int n = A.size();
+    vector<double> x(n, 0.0);
+    iter = 0;
+
+    cout << "\nSOR Iterations (w=" << w << "):\n";
+    while (true) {
+        vector<double> x_old = x;
+        cout << "Iteration " << iter + 1 << ": ";
+        for (int i = 0; i < n; i++) {
+            double sum = 0.0;
+            for (int j = 0; j < n; j++)
+                if (j != i) sum += A[i][j] * x[j];
+            x[i] = (1 - w) * x[i] + w * (b[i] - sum) / A[i][i];
+        }
